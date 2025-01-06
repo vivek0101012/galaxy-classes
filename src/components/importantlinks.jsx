3ba3770fe713551ab520e318072e5b9fd3a6432b
VIVEK NISHAD <vknitk2026@gmail.com>
2025-01-06 13:14:22 +0530

    added live

--- src/components/importantlinks.jsx
+++ src/components/importantlinks.jsx
@@ -30,7 +30,7 @@ export default function Links(){
    return <div className="py-16 bg-[#020817] ">
 
 <div className=" px-4 flex flex-row ">
-<img src="src/images/live.gif" width={24} alt="" />
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/LCzTq6S/live.gif" width={24} alt="live" border="0" /></a>
 <p className="text-white ">@Latest updates</p>
 </div>
 
